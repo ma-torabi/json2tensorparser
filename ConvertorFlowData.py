3cb231a1c13df478fc1a3be37c52101ed8835370
root <bluestar-mt>
2019-12-20 10:30:49 +0330

    Convert Flow feature to vector

--- ConvertorFlowData.py
+++ ConvertorFlowData.py
@@ -21,42 +21,45 @@ def getKeys( array, keyname, level, index):
             else:
                 kname  = keyname + '-' + k
             if not hasattr(v, "__len__") or isinstance(v, str):
-                if index == 1:
+                if (index == 1) and ('BF_label' not in kname):
                     vector[0].append(kname)
-                vector[index].append(v)
+                # lable must not be in the vector
+                if 'BF_label' not in kname :    
+                    vector[index].append(v)
             level +=1 
-            # print(kname)
             getKeys(v, kname, level,index)
     
 
 # Main Funtion
 def main():
+    i = 0
     # Load json file(s)
     path = 'sample-data/'
     for filename in glob.glob(os.path.join(path, '*.json')):
         with open(filename, 'r') as f:
             array = json.load(f)
-            i = 0
-            vector[i].append('tuple')
+            
+            if 'tuple' not in vector[0]:
+                vector[0].append('tuple')
 
             for (key, value) in array.items():
                 vector.append([])
                 i += 1
                 vector[i].append(key)
-                # fill vector 
-                # per flow/packet information
+
+                # fill vector  per flow/packet information
                 for (keytype, valuetype) in value.items():
                     level = 0
                     keyname = ''
                     #only check flow features
                     if (keytype == 'flow_features' or keytype == 'flow_metadata'):
                         getKeys(valuetype, keyname, level, i)
-            # print(vector)
+           
 
-            new_path = 'sample-data/flow-result'
-            flow_result = open(new_path,'w')
-            for row in vector:
-                flow_result.write("%s\n" % row)
+    new_path = 'sample-data/flow-result'
+    flow_result = open(new_path,'w+')
+    for row in vector:
+        flow_result.write("%s\n" % row)
 
 
 if __name__ == '__main__':
